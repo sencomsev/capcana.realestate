4f2f857cb6f68a1fcc3ebaba2dc2a564f743a926
sencomsev <sencomsev@gmail.com>
2025-08-29 22:28:09 -0400

    Create page.js

---
+++ src/app/maintenance/page.js
@@ -0,0 +1,22 @@
+export default function Maintenance() {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+      fontFamily: "sans-serif",
+      textAlign: "center",
+      background: "#f9f9f9"
+    }}>
+      <h1 style={{ fontSize: "2.5rem", marginBottom: "1rem" }}>
+        Website Under Construction
+      </h1>
+      <p style={{ fontSize: "1.2rem", color: "#555" }}>
+        We are working hard to bring you something amazing.<br />
+        Please check back soon!
+      </p>
+    </div>
+  );
+}
