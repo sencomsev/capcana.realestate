5431aeb9d6ddbf650a0f04dab0a29d969e53cb99
sencomsev <sencomsev@gmail.com>
2025-09-02 06:26:39 -0400

    Update page.jsx

--- src/app/preview-12345/page.jsx
+++ src/app/preview-12345/page.jsx
@@ -4,5 +4,5 @@ import Home_V10 from "../(home)/home-v10/page";
 export const dynamic = "force-static"; // можно убрать, просто на всякий
 
 export default function Preview() {
-  return <Home_V9 />;
+  return <Home_V10 />;
 }
