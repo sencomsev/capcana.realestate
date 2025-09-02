60da915dc9d24dcce9e3bdbf41d5644450f2cdef
sencomsev <sencomsev@gmail.com>
2025-09-02 06:13:00 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/((?!preview-12345).*)', // всё кроме preview-12345
-        destination: '/maintenance',
-        permanent: false,
-      },
-    ]
-  },
-}
-module.exports = nextConfig
+const nextConfig = {};
+module.exports = nextConfig;
+
