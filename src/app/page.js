79712c19ed24d8ff3574537b44ff8c6d3a75cdaa
sencomsev <sencomsev@gmail.com>
2025-09-10 14:56:55 -0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Home_V1 from "./(home)/home-v1/page";
 import Wrapper from "./layout-wrapper/wrapper";
 
 export const metadata = {
-  title: "Home v1 || Homez - Real Estate NextJS Template",
+  title: "Cap Cana Real Estate",
 };
 
 export default function MainRoot() {
@@ -12,3 +12,4 @@ export default function MainRoot() {
     </Wrapper>
   );
 }
+
